5984006aefd6851c2cd4b70fd7f8499311165986
Everett Vacek <everettvacek2020@u.northwestern.edu>
2017-03-02 16:19:06 -0600

    3/2/2017

--- Pilatus_cxi.py
+++ Pilatus_cxi.py
@@ -1,7 +1,7 @@
 import h5py
 import numpy as np
-from Pilatus_h5_proto import *
-import time
+from Pilatus_h5 import *
+import datetime
 
 path = 'C:/users/everett/research/hdf5/fly009'
 scan_meta = collect_tiff_meta(path)
@@ -19,11 +19,12 @@ entry_1 = f.create_group('entry_1')
 
 sample_1 = entry_1.create_group('sample_1')
 geometry_1 = sample_1.create_group('geometry_1')
+beam_xy = geometry_1.create_dataset('beam_xy', data = scan_meta['001']['Beam_xy'][0])
 
 image_1 = entry_1.create_group('image_1')
 #data = image_1.create_dataset('data')
 process_1 = image_1.create_group('process_1')
-date = process_1.create_dataset('date', data = time.asctime( time.localtime(time.time()) ))
+date = process_1.create_dataset('date', data = datetime.datetime.now().isoformat())
 note_1 = process_1.create_group('note_1')
 for line in scan_meta:
 	folder = note_1.create_group('metadata_' + line)
@@ -33,12 +34,21 @@ for line in scan_meta:
 
 translation = geometry_1.create_dataset('translation', data = translations) #256x3 array of translations (m)
 translation.attrs['Units'] = 'meters'
+angle_increment = geometry_1.create_dataset('angle_increment', data = scan_meta['001']['Angle_increment'][0])
+
+for line in scan_meta:
+	start_angle = geometry_1.create_dataset('start_angle_' + line, data = scan_meta[line]['Start_angle'])
+
 
 data_1 = entry_1.create_group('data_1')
 data_1["translation"] = h5py.SoftLink('/entry_1/sample_1/geometry_1/translation')
 
 instrument_1 = entry_1.create_group('instrument_1')
 source_1 = instrument_1.create_group('source_1')
+flux = source_1.create_dataset('flux', data = scan_meta['001']['Flux'][0])
+attenuator_1 = instrument_1.create_group('attenuator_1')
+filter_transmission = attenuator_1.create_dataset('filter_transmission', data = scan_meta['001']['Filter_transmission'])
+
 energy_range = source_1.create_dataset('incident_energy_range', data = scan_meta['001']['Energy_range'][0])
 wavelength = source_1.create_dataset('incident_wavelength', data = scan_meta['001']['Wavelength'][0])
 polarization = source_1.create_dataset('incident_polarization', data = scan_meta['001']['Polarization'][0])
@@ -46,7 +56,8 @@ detector_1 = instrument_1.create_group('detector_1')
 detector_1['translation'] = h5py.SoftLink('/entry_1/sample_1/geometry_1/translation')
 distance = detector_1.create_dataset('distance', data = scan_meta['001']['Detector_distance'][0])
 count_cutoff = detector_1.create_dataset('count_cutoff', data = scan_meta['001']['Count_cutoff'][0])
-gain_setting = detector_1.create_dataset('Gain_setting:', data = scan_meta['001']['Gain_setting:'][0])
+gain_setting = detector_1.create_dataset('gain_setting', data = scan_meta['001']['Gain_setting:'][0])
+v_offset = detector_1.create_dataset('v_offset', data = scan_meta['001']['Detector_Voffset'][0])
 
 
 for line in scan_data:
--- Pilatus_h5.py
+++ Pilatus_h5.py
@@ -4,7 +4,7 @@ import os
 import tifffile as tf
 import h5py
 
-__all__ = [ 'Pilatus_to_hdf5' ]
+#__all__ = [ 'Pilatus_to_hdf5' ]
 
 class cd:
     """Context manager for changing the current working directory"""
@@ -22,61 +22,86 @@ class cd:
 
 def collect_tiff_meta(dir):
 	"""Collects tiff metadata and loads it into a python dictionary"""
+	with cd(dir):
 
-	#create dictionary
-	scan_meta = {}
-	scan_line = list(sorted(set([filename[7:10] for filename in os.listdir(dir) if filename.endswith('.tif')])))
+		#create dictionary
+		scan_meta = {}
+		scan_line = list(sorted(set([filename[7:10] for filename in os.listdir(dir) if filename.endswith('.tif')])))
 
-	#search directory for tif files and collect metadata
-	for line in scan_line:
-		#create sub-dictionary for each scanline
-		important_meta = {'Filename': [], 'Pixel_size': [], 'Exposure_time': [], 'Exposure_period': [],
-						'Count_cutoff': [], 'Threshold_setting:': [], 'Image_path:': [],
-						'Retrigger_mode:': [], 'Wavelength': [], 'Detector_distance': [],
-						'Detector_Voffset': [], 'Beam_xy': [], 'Flux': [], 'Start_angle': [],
-						'Angle_increment': []}
-		for filename in os.listdir(dir):
-			if filename.endswith(".tif") and filename.startswith(line,7):
-				name = os.path.splitext(filename)[0]
-				
-				#get image structure information 
-				image = tf.imread(filename)
-				
-				#read auxillary metadata
-				file = open(filename, 'rb')
-				data = file.read()
-				
-				#store data in dictionary
-				important_meta['Filename'].append(name)
-				for key in important_meta:
-					if key == 'Filename':
-						pass
-					else:
-						start = data.find(key)+len(key)+1
-						end = data.find('\r\n', start)
-						important_meta[key].append(data[start:end])
-		scan_meta[line] = important_meta
-	return scan_meta
+		#search directory for tif files and collect metadata
+		for line in scan_line:
+			#create sub-dictionary for each scanline
+			important_meta = {
+							'Filename': [], 'Pixel_size': [], 'Silicon sensor, thickness' : [], 'Exposure_time': [], 'Exposure_period': [],
+							'Tau' : [], 'Count_cutoff': [], 'Threshold_setting:': [], 'Gain_setting:' : [], 'N_excluded_pixels' : [], 
+							'Excluded_pixels:' : [], 'Flat_field:' : [], 'Trim_file:' : [], 'Image_path:': [], 'Ratecorr_lut_directory:' : [], 
+							'Retrigger_mode:': [], 'Wavelength': [], 'Energy_range': [], 'Detector_distance': [], 'Detector_Voffset': [],
+							'Detector_Voffset': [], 'Beam_xy': [], 'Flux': [], 'Filter_transmission' : [], 'Start_angle': [], 'Detector_2theta' : [],
+							'Angle_increment': [], 'Polarization' : [], 'Alpha' : [], 'Kappa' : [], 'Phi' : [], 'Phi_increment' : [], 'Chi' : [],
+							'Chi_increment' : [], 'Oscillation_axis' : [], 'N_oscillations' : []
+							}
+			for filename in os.listdir(dir):
+				if filename.endswith(".tif") and filename.startswith(line,7):
+					name = os.path.splitext(filename)[0]
+					
+					#get image structure information 
+					image = tf.imread(filename)
+					
+					#read auxillary metadata
+					file = open(filename, 'rb')
+					data = file.read()
+					
+					#store data in dictionary
+					important_meta['Filename'].append(name)
+					for key in important_meta:
+						if key == 'Filename':
+							pass
+						else:
+							start = data.find(key)+len(key)+1
+							end = data.find('\r\n', start)
+							important_meta[key].append(data[start:end])
+			scan_meta[line] = important_meta
+		return scan_meta
 
 def collect_tiff_data(dir):
 	"""Collects tiff data and loads it into python dictionary"""
-	
-	scan_line = list(sorted(set([filename[7:10] for filename in os.listdir(dir) if filename.endswith('.tif')])))
-	scan_data = {}
+	import numpy as np
+	with cd(dir):
 
-	for line in scan_line:
-		scan_line_arrays = {}
-		for filename in os.listdir(dir):
-			if filename.endswith(".tif") and filename.startswith(line, 7):
-				name = os.path.splitext(filename)[0]
-				
-				data_array = tf.imread(filename)
-				scan_line_arrays[filename] = data_array
-		scan_data[line] = scan_line_arrays
+		scan_line = list(sorted(set([filename[7:10] for filename in os.listdir(dir) if filename.endswith('.tif')])))
+		scan_data = {}
+		for line in scan_line:
+			data_array = []
+			#scan_line_arrays = {}
+			for filename in os.listdir(dir):
+				if filename.endswith(".tif") and filename.startswith(line, 7):
+					name = os.path.splitext(filename)[0]
+					data_array.append(tf.imread(filename))
+					#data_array = tf.imread(filename) 
+					#scan_line_arrays[filename] = data_array
+			#scan_data[line] = scan_line_arrays
+			scan_data[line] = np.asarray(data_array, order = 'C')
 	return scan_data	
 
-def Pilatus_to_hdf5(dir):
+"""	
+def collect_tiff_data_cix(dir):
+	import numpy as np
+	with cd(dir):
+		
+		scan_line = list(sorted(set([filename[7:10] for filename in os.listdir(dir) if filename.endswith('.tif')])))
+		scan_data = np.ndarray( shape
+		for line in scan_line:
+			for filename in os.listdir(os.getcwd()):
+				if filename.endswith(".tif") and filename.startswith(line, 7):
+					name = os.path.splitext(filename)[0]
+					data_array = tf.imread(filename)
+					scan_line_arrays[filename] = data_array
+"""
+
+
+'''def Pilatus_to_hdf5(dir):
 	"""
+	USE Pilatus_cxi.py INSTEAD
 	Uses data and metadata dictionaries to construct HDF5 file
 	
 	Parameters
@@ -125,3 +150,4 @@ def Pilatus_to_hdf5(dir):
 
 		# Close the file before exiting
 		file.close()
+'''
