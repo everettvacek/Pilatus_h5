3e9c0cae23f31827b9283a0fc39058871802c631
Everett Vacek <everett@xrm.phys.northwestern.edu>
2017-06-13 11:48:36 -0500

    first working version of file per line

--- Pilatus_tiff.py
+++ Pilatus_tiff.py
@@ -83,26 +83,29 @@ def collect_tiff_data(line):
 	scan_data[line] = np.asarray(data_array, order = 'C')
 	return scan_data
 
-def create_cxi():
+def create_cxi(dir = None):
 
 	import datetime
+	import numpy as np
 	
-	dir = input('path: ')
+	if dir == None:
+		dir = '/raid/home/everett/Pilatus_h5/fly009/'
+	else:
+		dir = input('path: ')
 	with cd(dir):
 		
 		scan_line = list(sorted(set([filename[7:10] for filename in os.listdir(dir) if filename.endswith('.tif') and filename.startswith('fly')])))
 
 
 		for line in scan_line:
-
+			
 			scan_meta = collect_tiff_meta(line)
 			scan_data = collect_tiff_data(line)
-
 			filename = scan_meta[line]['Filename'][0][0:10]+'.cxi'
 			f = h5py.File(filename ,'w')
 			f.create_dataset('cxi_version', data = 150)
 
-			num_images = len(scan_meta['line']['Filename']) #??????????????????????????????????????????????????????????????????????????????????????????????????
+			num_images = len(scan_meta[line]['Filename']) #??????????????????????????????????????????????????????????????????????????????????????????????????
 			tiff = np.ndarray(shape = (num_images,619,487), dtype = float)
 			translations = np.ndarray(shape = (num_images,3), dtype = float)
 
@@ -111,7 +114,7 @@ def create_cxi():
 
 			sample_1 = entry_1.create_group('sample_1')
 			geometry_1 = sample_1.create_group('geometry_1')
-			beam_xy = geometry_1.create_dataset('beam_xy', data = scan_meta['001']['Beam_xy'][0])
+			beam_xy = geometry_1.create_dataset('beam_xy', data = scan_meta[line]['Beam_xy'])
 
 			image_1 = entry_1.create_group('image_1')
 
@@ -127,7 +130,7 @@ def create_cxi():
 
 			translation = geometry_1.create_dataset('translation', data = translations) #256x3 array of translations (m)
 			translation.attrs['Units'] = 'meters'
-			angle_increment = geometry_1.create_dataset('angle_increment', data = scan_meta['001']['Angle_increment'][0])
+			angle_increment = geometry_1.create_dataset('angle_increment', data = scan_meta[line]['Angle_increment'])
 
 #			for line in scan_meta:
 			start_angle = geometry_1.create_dataset('start_angle_' + line, data = scan_meta[line]['Start_angle'])
@@ -138,20 +141,20 @@ def create_cxi():
 
 			instrument_1 = entry_1.create_group('instrument_1')
 			source_1 = instrument_1.create_group('source_1')
-			flux = source_1.create_dataset('flux', data = scan_meta['001']['Flux'][0])
+			flux = source_1.create_dataset('flux', data = scan_meta[line]['Flux'])
 
 			attenuator_1 = instrument_1.create_group('attenuator_1')
-			filter_transmission = attenuator_1.create_dataset('filter_transmission', data = scan_meta['001']['Filter_transmission'])
+			filter_transmission = attenuator_1.create_dataset('filter_transmission', data = scan_meta[line]['Filter_transmission'])
 
-			energy_range = source_1.create_dataset('incident_energy_range', data = scan_meta['001']['Energy_range'][0])
-			wavelength = source_1.create_dataset('incident_wavelength', data = scan_meta['001']['Wavelength'][0])
-			polarization = source_1.create_dataset('incident_polarization', data = scan_meta['001']['Polarization'][0])
+			energy_range = source_1.create_dataset('incident_energy_range', data = scan_meta[line]['Energy_range'])
+			wavelength = source_1.create_dataset('incident_wavelength', data = scan_meta[line]['Wavelength'])
+			polarization = source_1.create_dataset('incident_polarization', data = scan_meta[line]['Polarization'])
 			detector_1 = instrument_1.create_group('detector_1')
 			detector_1['translation'] = h5py.SoftLink('/entry_1/sample_1/geometry_1/translation')
-			distance = detector_1.create_dataset('distance', data = scan_meta['001']['Detector_distance'][0])
-			count_cutoff = detector_1.create_dataset('count_cutoff', data = scan_meta['001']['Count_cutoff'][0])
-			gain_setting = detector_1.create_dataset('gain_setting', data = scan_meta['001']['Gain_setting:'][0])
-			v_offset = detector_1.create_dataset('v_offset', data = scan_meta['001']['Detector_Voffset'][0])
+			distance = detector_1.create_dataset('distance', data = scan_meta[line]['Detector_distance'])
+			count_cutoff = detector_1.create_dataset('count_cutoff', data = scan_meta[line]['Count_cutoff'])
+			gain_setting = detector_1.create_dataset('gain_setting', data = scan_meta[line]['Gain_setting:'])
+			v_offset = detector_1.create_dataset('v_offset', data = scan_meta[line]['Detector_Voffset'])
 
 			#image data is stored here
 
@@ -166,9 +169,9 @@ def create_cxi():
 #			image_ref = detector_1.create_dataset('scan_line_ref', (0,), dtype = ref_dtype,  maxshape = (len(scan_data[line]))
 
 			log_1 = detector_1.create_group('log')
-			Retrigger_mode = log_1.create_dataset('retrigger_mode', data = int(scan_meta['001']['Retrigger_mode:'][0]))
-			exposure_time = log_1.create_dataset('exposure_time', data = scan_meta['001']['Exposure_time'][0])
-			exposure_period = log_1.create_dataset('exposure_period', data = scan_meta['001']['Exposure_period'][0])
+			Retrigger_mode = log_1.create_dataset('retrigger_mode', data = [int(i) for i in scan_meta[line]['Retrigger_mode:']])
+			exposure_time = log_1.create_dataset('exposure_time', data = scan_meta[line]['Exposure_time'])
+			exposure_period = log_1.create_dataset('exposure_period', data = scan_meta[line]['Exposure_period'])
 
 
 #			for line in scan_data:
