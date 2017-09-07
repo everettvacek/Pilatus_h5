e354395a66c097343a40504a3e778a37705e92fe
Everett Vacek <everett@xrm.phys.northwestern.edu>
2017-09-07 14:09:56 -0500

    changed to fit Pilatus_tiff.py functions

--- Pilatus_cxi.py
+++ Pilatus_cxi.py
@@ -1,11 +1,12 @@
 import Pilatus_tiff as pt
 import os
 
+
 path = os.getcwd() + '/fly009'
 
-#Create a .cxi file per scanline
-#pt.create_cxi(path)
+#Create an .h5 file per scanline
+pt.create_line_h5(path)
 
 #Create master .cxi  file with links to each scanline
 #If overwrite is True all entries will be deleted
-pt.create_master(path, overwrite = True)
+pt.create_master(path)
